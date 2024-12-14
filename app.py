1b789467de63523606fb41d92fe2770740a3fce1
Lohith <69358508+lohithgsk@users.noreply.github.com>
2024-12-14 21:52:38 +0530

    moved fs initialization to begining

--- app.py
+++ app.py
@@ -81,6 +81,7 @@ app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
 jwt = JWTManager(app)
 CORS(app)
+fs = gridfs.GridFS(mongo.db)
 
 mongo = PyMongo(app)
 # BASE_URL = "https://api.gms.intellx.in"
@@ -2010,8 +2011,6 @@ def generate_pdf():
     pdf.save()
     buffer.seek(0)
     return Response(buffer, mimetype='application/pdf', headers={"Content-Disposition": "inline;filename=dynamic_report.pdf"})
-
-fs = gridfs.GridFS(mongo.db)
 
 
 @app.route('/raise_lost_item', methods=['POST'])
